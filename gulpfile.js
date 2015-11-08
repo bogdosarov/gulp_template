55d4fcf7f93ec609bad6f3edf623d770d789e11a
Igor Bogdosarov <igor.bogdosarov@gmail.com>
2015-11-08 21:10:45 +0200

    Init project

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    autoprefix = require('gulp-autoprefixer');
+
+var src = {
+    less: 'app/styles/**/*.less',
+    css:  'app/styles/compiled',
+    html: 'app/*.html'
+};
+
+gulp.task('css', function () {
+    console.log('compile css');
+    gulp.src(src.less)
+        .pipe(less())
+        .pipe(autoprefix('last 3 version'))
+        .pipe(gulp.dest(src.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('browser-sync', function () {
+    var files = [
+        'app/styles/**/*.less',
+    ];
+
+    browserSync.init(files, {
+        server: {
+            baseDir: './app'
+        }
+    });
+});
+
+gulp.task('serve', ['css'], function() {
+
+    browserSync({
+        server: "./app"
+    });
+
+    gulp.watch(src.less, ['css']);
+    gulp.watch(src.html).on('change', reload);
+});
